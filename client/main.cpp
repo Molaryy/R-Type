499815a20473ac64378a42f989c45f681a868317
moonia <mounia.arjdal@epitech.eu>
2024-12-02 19:10:55 +0100

    fix: sfml into raylib

--- client/main.cpp
+++ client/main.cpp
@@ -1,24 +1,14 @@
-#include <SFML/Graphics.hpp>
+#include <raylib.h>
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
-
-  while (window.isOpen())
-  {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      if (event.type == sf::Event::Closed)
-        window.close();
+    InitWindow(800, 600, "Raylib Window");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, Raylib works !", 200, 200, 200, ORANGE);
+        EndDrawing();
     }
-
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-
-  return 0;
+    CloseWindow();
+    return 0;
 }
