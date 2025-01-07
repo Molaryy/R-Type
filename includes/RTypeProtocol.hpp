b7f2de60f41b0c0e582c2fe5dbbf9a314b382c09
Tchips46tek <leo.outmizguine@epitech.eu>
2025-01-07 01:56:49 +0100

    feat: rtype base protocol

--- includes/RTypeProtocol.hpp
+++ includes/RTypeProtocol.hpp
@@ -63,25 +63,24 @@ namespace Protocol {
     };
 
     enum CommandIdClient : uint16_t {
-        CONNECT,
-        CLIENT_ASK_START_GAME,
-        INPUT_KEYS,
-        DISCONNECT,
+        CONNECT, // Client send this packet at startup
+        CLIENT_ASK_START_GAME, // Ask for game start, will start game for the actual lobby
+        INPUT_KEYS, // PacketInputsKeysPacket, all inputs from clients
+        DISCONNECT, // Disconnection
     };
 
     enum CommandIdServer : uint16_t {
-        ACCEPT_CONNECTION,
-        START_GAME,
-        POSITION_VELOCITY,
-        SPAWN,
-        HIT,
-        KILL,
-        CAMERA_MOVE,
-        END_GAME,
+        ACCEPT_CONNECTION, // AcceptConnectionPacket, response to Client CONNECT
+        START_GAME, // Game is starting, server will start sending game state
+        POSITION_VELOCITY, // EntityPositionVelocityPacket, state of a new entity on screen
+        SPAWN, // SpawnEntityPacket, data about a new entity to be displayed
+        HIT, // HitPacket, entity got hit
+        KILL, // DeadPacket, entity is dead because 0 helth or out of screen
+        END_GAME, // Game is over
         SERVER_SHUTDOWN,
     };
 
-    struct Empty {
+    struct EmptyPacket {
     };
 
     struct Vector2i {
@@ -89,39 +88,36 @@ namespace Protocol {
         int y;
     };
 
-    struct AcceptConnection {
+    struct AcceptConnectionPacket {
         std::size_t entity_id;
     };
 
-    struct SpawnEntity {
+    struct SpawnEntityPacket {
         std::size_t entity_id;
         EntityType type;
         Vector2i position;
         Vector2i velocity;
     };
 
-    struct EntityPositionVelocity {
+    struct EntityPositionVelocityPacket {
         std::size_t entity_id;
         Vector2i position;
         Vector2i velocity;
     };
 
-    struct Hit {
+    struct HitPacket {
         std::size_t entity_id1;
         std::size_t entity_id2;
+        int health_entity1;
+        int health_entity2;
     };
 
-    struct Dead {
+    struct DeadPacket {
         std::size_t entity_id;
         bool natural;
     };
 
-    struct CameraMove {
-        Vector2i position;
-        Vector2i velocity;
-    };
-
-    struct PacketInputsKeys {
+    struct PacketInputsKeysPacket {
         bool input_keys[NB_INPUTS_KEYS];
     };
 }
