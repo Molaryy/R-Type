079d4ef773971ea69d53c511f4bfe0d8f006d10c
Molaryy <molaryyios@gmail.com>
2025-01-11 04:30:47 +0100

    feat: adde basic stuff for menu ECS

--- client/includes/Systems.hpp
+++ client/includes/Systems.hpp
@@ -7,31 +7,34 @@
 
 #pragma once
 
-#include <cstddef>
-
 #include "Client.hpp"
-#include "Entity.hpp"
 #include "INetworkClient.hpp"
 #include "PacketHandler.hpp"
-#include "RTypeProtocol.hpp"
 #include "Registry.hh"
+#include "Zipper.hh"
+#include "Components.hpp"
+#include "Components.hh"
 
-namespace System {
+namespace Systems {
     inline void networkReceiver([[maybe_unused]] Registry &r) {
-        auto &core = R_Type::Client::getInstance();
+        auto &core = Client::getInstance();
         auto &network = core.getNetworkLib();
         auto &packet_handler = core.getPacketHandler();
 
-        std::vector<uint8_t> oldest_packet = network.getOldestPacket();
-
-        while (!oldest_packet.empty()) {
-            ::Network::Packet deserialized_packet(oldest_packet);
+        for (std::vector<uint8_t> oldest_packet = network.getOldestPacket(); !oldest_packet.empty(); oldest_packet = network.getOldestPacket()) {
+            Network::Packet deserialized_packet(oldest_packet);
             packet_handler(deserialized_packet);
+        }
+    }
 
-            if (deserialized_packet.getCommandId() == R_Type::START_GAME) {
-                break;
-            }
-            oldest_packet = network.getOldestPacket();
+    inline void drawAllTexts(Registry &r) {
+        auto &texts = r.get_components<Components::RenderText>();
+        auto &colors = r.get_components<Components::ColorText>();
+        Graphic::IRenderer &renderer = Client::getInstance().getRenderer();
+
+        for (auto &&[text, color] : Zipper(texts, colors)) {
+            std::cout << "Drawing text: " << text.text << std::endl;
+            renderer.drawText(text.text, text.x, text.y, text.fontSize, color.r, color.g, color.b, color.a);
         }
     }
 }
